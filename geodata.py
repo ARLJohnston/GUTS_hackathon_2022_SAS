2d2aec87af36f414c21f98752c21c208beabf931
Miguel <miguel.lameg23@gmail.com>
2022-10-14 23:49:15 +0100

    found a way to programmatically add generate heatmaps

--- geodata.py
+++ geodata.py
@@ -2,12 +2,13 @@ import geopandas as gpd
 import matplotlib as plt
 import pandas as pd
 import folium
-import io
+
 from PIL import Image
 from selenium import webdriver
 import os
 import time
 import requests
+from folium.plugins import HeatMap
 
 BASE_URL = 'https://nominatim.openstreetmap.org/search?format=json'
 
@@ -24,12 +25,25 @@ OSmap = folium.Map(location = loc,
 
 OSmap.save("map.html")
 
-h_file = "map.html"
+def generate_map_image(x,y):
+    temp = 'file://{path}/{mapfile}'.format(path=os.getcwd(),mapfile=x)
+    browser = webdriver.Firefox()
+    browser.get(temp)
 
-temp = 'file://{path}/{mapfile}'.format(path=os.getcwd(),mapfile=h_file)
-browser = webdriver.Firefox()
-browser.get(temp)
+    time.sleep(3)
+    browser.save_screenshot(y)
+    browser.quit()
 
-time.sleep(5)
-browser.save_screenshot('map.png')
-browser.quit()
+def generate_heat_map(lat,long):
+    geo_map = folium.Map(location=[lat,long],zoom_start=18)
+
+    tmp_arr = []
+    tmp_arr.append([lat,long])
+
+    HeatMap(tmp_arr).add_to(geo_map)
+
+    fname= 'heatmap.html'
+    geo_map.save(fname)
+    generate_map_image(fname,'heatmap.png')
+
+generate_heat_map(latitude,longitude)
