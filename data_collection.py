ff5b6e4912ecab20bfbeac7aaec802e169abe28c
2469916m <2469916m@student.gla.ac.uk>
2022-10-14 20:10:09 +0100

    5. Added getters for security_logs

--- data_collection.py
+++ data_collection.py
@@ -7,6 +7,9 @@ location_data = pd.read_csv("location_data.csv", on_bad_lines='warn')
 people_data = pd.read_csv("people_data.csv", on_bad_lines='warn')
 security_data = pd.read_csv("security_logs.csv", on_bad_lines='warn')
 
+
+
+
 # Helper Functions regarding location data
 
 # Location Data row getters
