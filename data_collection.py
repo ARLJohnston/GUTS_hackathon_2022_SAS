3c43ac8e5ee6f6e14488f9f20f87302437a148b9
2469916m <2469916m@student.gla.ac.uk>
2022-10-14 19:33:10 +0100

    4. Added getters for people_data

--- data_collection.py
+++ data_collection.py
@@ -11,20 +11,46 @@ security_data = pd.read_csv("security_logs.csv", on_bad_lines='warn')
 
 # Location Data row getters
 # returns all datasets as NumPy arrays
-def get_building_names():
+def get_location_building_names():
     return location_data['Building Name'].to_numpy()
 
-def get_geolocations():
+def get_location_geolocations():
     return location_data['Geolocation'].to_numpy()
 
-def get_opening_times():
+def get_location_opening_times():
     return location_data['Opening Times'].to_numpy()
 
-def get_description():
+def get_location_description():
     return location_data['Description'].to_numpy()
 
-b = get_building_names()
-g = get_geolocations()
-o = get_opening_times()
-d = get_description()
+
+# Getter functions for the attributes in people data
+def get_people_student_id():
+    return people_data['Student ID'].to_numpy()
+
+def get_people_name():
+    return people_data['Name'].to_numpy()
+def get_people_age():
+    return people_data['Age'].to_numpy()
+
+def get_people_sex():
+    return people_data['Sex'].to_numpy()
+
+def get_people_year_of_study():
+    return people_data['Year of Study'].to_numpy()
+
+def get_people_subject():
+    return people_data['Subject'].to_numpy()
+
+def get_people_height():
+    return people_data['Height'].to_numpy()
+
+def get_people_hair_colour():
+    return people_data['Hair colour'].to_numpy()
+
+def get_people_societies():
+    return people_data['Societies'].to_numpy()
+
+
+
 
