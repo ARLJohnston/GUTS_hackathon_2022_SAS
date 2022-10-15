accd739059535dbbe9f36fccca24fa21bc85680b
2469916m <2469916m@student.gla.ac.uk>
2022-10-15 12:20:37 +0100

    10. merged data_collection and data_collection_2

--- data_collection.py
+++ data_collection.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from certifi import where
 
 # Load the three data files for use in later functions
 location_data = pd.read_csv("location_data.csv", on_bad_lines='warn')
@@ -172,3 +173,22 @@ def times_match(student_id):
             return False
 
     return True
+
+
+def get_point_time(time):
+    student_times = security_data.copy()
+    student_times[['Start', 'End']] = student_times['Time'].str.split('-', 1, expand=True).to_numpy()
+    student_times = student_times.drop('Time', axis=1)
+    student_times['Start'] = student_times['Start'].astype(int)
+    student_times['End'] = student_times['End'].astype(int)
+
+    student_times_array = student_times.to_numpy()
+    # print(student_times_array)
+    next_day_mask = np.array(student_times_array[:, 3] > student_times_array[:, 4])
+    student_times_array[next_day_mask, 4] += 2400
+    print(student_times_array.shape)
+    # student_times_array[:,4] = dc.np.where(,student_times_array[:,4],student_times_array[:,4]+2400)
+
+    mask = np.where((student_times_array[:, 3] < time) & (student_times_array[:, 4] > time))
+
+    return student_times_array[mask]
